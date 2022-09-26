bca8fdbe2539cadcd8f7e34ec7f73e4fed4b10a8
Bhagat, Vikas <vikas.bhagat@hilti.com>
2022-09-26 14:27:51 +0200

    Hello World

---
+++ src/main/java/com/example/eb2bpublicstockapi/controller/StockController.java
@@ -0,0 +1,35 @@
+package com.example.eb2bpublicstockapi.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/stock")
+@RestController
+public class StockController {
+
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(StockController.class);
+
+
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public  @ResponseBody String requestCatalog() {
+
+        return "Hello World";
+    }
+
+
+
+
+
+}
