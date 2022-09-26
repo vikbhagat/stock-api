e3bb3c32f0bdb90bdf6b6e958a09f133a41e7a58
Bhagat, Vikas <vikas.bhagat@hilti.com>
2022-09-26 13:53:37 +0200

    Intial commit

---
+++ src/main/java/com/example/eb2bpublicstockapi/Eb2bPublicStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eb2bpublicstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eb2bPublicStockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eb2bPublicStockApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eb2bpublicstockapi/Eb2bPublicStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eb2bpublicstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eb2bPublicStockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
