ea02128d89b21ce7a3008295686b6feffbae7e8b
Josh Murphy <joshua.murphy@hilti.com>
2022-09-27 14:14:42 +0200

    Pricing service spoof update

--- src/main/java/com/example/eb2bpublicstockapi/service/PricingService.java
+++ src/main/java/com/example/eb2bpublicstockapi/service/PricingService.java
@@ -14,20 +14,18 @@ public class PricingService {
         JSONObject priceResponse = new JSONObject();
         JSONObject itemInfo = new JSONObject();
         JSONObject priceType = new JSONObject();
-        Double listPrice = rand.nextDouble();
+        Double listPrice = rand.nextDouble()*10;
         Double listPriceQuant = rand.nextInt(20)*5.0;
 
         priceResponse.put("material_number",itemNumber);
-        itemInfo.put("list_price", rand.nextDouble());
-        itemInfo.put("materialN", rand.nextDouble());
         itemInfo.put("status", 200);
         itemInfo.put("net_price", 0.03);
         itemInfo.put("unit_net_price", 3.0);
         itemInfo.put("sales_uom", "ST");
         itemInfo.put("quantity", 1.0);
         itemInfo.put("minimum_order_quantity", 25.0);
-        itemInfo.put("list_price", listPrice);
-        itemInfo.put("list_price_quantity", listPriceQuant);
+        itemInfo.put("list_price", listPrice.toString());
+        itemInfo.put("list_price_quantity", listPriceQuant.toString());
         itemInfo.put("list_price_uom", "ST");
         itemInfo.put("uom_numerator", 1);
         itemInfo.put("uom_denominator", 1);
